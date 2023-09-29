efb1b60b0de031a6c6dd159e11802272cc2d2aaf
uyentruong2003 <uyenkaty.truong@gmail.com>
2023-09-29 12:33:31 -0500

    clone

---
+++ myFirstChatbot.py
@@ -0,0 +1,23 @@
+import openai
+import streamlit as st
+openai.api_key="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"
+
+def generate_response(myprompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        # ChatGPT role:
+        prompt= "Your role is to act as a licensed therapist ###"+ myprompt,
+        temperature=.1,
+        max_tokens=1024
+    )
+    print (response.choices)
+    return response.choices[0].text.strip()
+
+def main ():
+    st.title("Friendly Chat with Dr. Truong")
+    myprompt = st.input("I am Dr. Truong. I am a licensed Therapist. How can I help you? \n")
+    if st.button("Submit"):
+        st.write(generate_response(myprompt))
+
+if __name__ == "__main__":
+    main()
